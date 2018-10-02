2240f678014f25cb08b1b1336b9d499bdf24b423
velansalis <velanmax8@gmail.com>
2018-10-02 19:30:47 +0530

    Changed stack to fcc

--- dist/js/index.js
+++ dist/js/index.js
@@ -5,30 +5,30 @@ const newText = document.querySelector("#newText");
 
 // Primary Text
 function displayText() {
-	let welcomeString = `Hey there!^ You People on the Internet,^Its Velan here :)`,
-		i = 0;
-	window.setInterval(function() {
-		if (welcomeString.charAt(i) == "^") {
-			title.innerHTML += "<br>";
-			i++;
-		}
-		if (welcomeString.length == i) {
-			doneShowing();
-		}
-		title.innerHTML += welcomeString.charAt(i);
-		i++;
-	}, 175);
+  let welcomeString = `Hey there!^ You People on the Internet,^Its Velan here :)`,
+    i = 0;
+  window.setInterval(function() {
+    if (welcomeString.charAt(i) == "^") {
+      title.innerHTML += "<br>";
+      i++;
+    }
+    if (welcomeString.length == i) {
+      doneShowing();
+    }
+    title.innerHTML += welcomeString.charAt(i);
+    i++;
+  }, 175);
 }
 
 // Callback text
 function doneShowing() {
-	let string = "things",
-		i = 0;
-	strikeText.setAttribute("style", "text-decoration:line-through");
-	window.setInterval(function() {
-		newText.innerHTML += string.charAt(i);
-		i++;
-	}, 175);
+  let string = "things",
+    i = 0;
+  strikeText.setAttribute("style", "text-decoration:line-through");
+  window.setInterval(function() {
+    newText.innerHTML += string.charAt(i);
+    i++;
+  }, 175);
 }
 
 displayText();
